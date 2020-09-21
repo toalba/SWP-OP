44c314030fd4ab055dd0868ee61efcb8ec80a39a
toalba <tb@kleinundpartner.at>
2020-09-21 10:57:30 +0200

    first commit

---
+++ Aufgabe1-Binarysearch/Binarysearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aufgabe1_Binarysearch
+{
+    class Binarysearch
+    {
+        public void bsearcher()
+        {
+
+        }
+        public void lsearcher(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+            }
+
+        }
+        //Today scripting in C#, because its new for me and I want to have a learn effect
+        static void Main(string[] args)
+        {
+            int lenght = 100;
+            int[] sarray = new int[lenght];
+
+            Array.Sort(sarray);
+
+
+        }
+    }
+}
