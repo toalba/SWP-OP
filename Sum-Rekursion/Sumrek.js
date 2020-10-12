2303b2094d922ddefba5354b8c294e517112cc44
toalba <tb@kleinundpartner.at>
2020-10-12 10:00:12 +0200

    sumita

---
+++ Sum-Rekursion/Sumrek.js
@@ -0,0 +1,17 @@
+function berechnersumrek(wert) {
+    if (wert>0) {
+        return wert +=berechnersumrek(wert-1)
+    }
+    else return 0
+}
+function berechnersumita(wert) {
+    var sum = 0
+    while (wert>0) {
+        sum+=wert
+        wert-=1
+    }
+    return sum
+}
+
+console.log(berechnersumrek(10))
+console.log(berechnersumita(10))
